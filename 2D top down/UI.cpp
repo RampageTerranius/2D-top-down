2e586b7ca6970af0f62e87ffdd724b37061e588f
Tyler Brown <rampage_terranius@hotmail.com>
2020-06-17 14:06:28 +1000

    Removed unnecessary code from UI class

--- 2D top down/UI.cpp
+++ 2D top down/UI.cpp
@@ -16,19 +16,8 @@ bool UI::Render(int rotationAngle)
 		rect.x -= static_cast<int> (round(static_cast<float> (rect.w / 2)));
 		rect.y -= static_cast<int> (round(static_cast<float> (rect.h / 2)));
 
-		if (xRotLoc < 0 || yRotLoc < 0)
-		{
-			if (SDL_RenderCopyEx(mainRenderer, texture->Tex(), NULL, &rect, rotationAngle, NULL, SDL_FLIP_NONE) >= 0)
-				return true;
-		}
-		else
-		{
-			SDL_Point rotpoint;
-			rotpoint.x = xRotLoc;
-			rotpoint.y = yRotLoc;
-			if (SDL_RenderCopyEx(mainRenderer, texture->Tex(), NULL, &rect, rotationAngle, &rotpoint, SDL_FLIP_NONE) >= 0)
-				return true;
-		}
+		if (SDL_RenderCopyEx(mainRenderer, texture->Tex(), NULL, &rect, rotationAngle, NULL, SDL_FLIP_NONE) >= 0)
+			return true;		
 	}
 
 	debug.Log("Entity", "Render", "failed to render a UI object");
--- 2D top down/UI.h
+++ 2D top down/UI.h
@@ -4,11 +4,11 @@
 
 #include "Entity.h"
 
+// UI class is relative to the screen.
+// This class is best used for objects that need to be at a specific coordinate on the screen reguardless of map.
+
 class UI : public Entity
 {
 public:
 	bool Render(int rotationAngle);
-
-	int xRotLoc = -1;
-	int yRotLoc = -1;
 };
